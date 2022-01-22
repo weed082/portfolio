19669f36d1520f32f36d2f60e1ae7032cf9dedda
ByungHakNoh <nbh6579@naver.com>
2022-01-22 21:16:47 +0900

    [UPDATE]

---
+++ src/component/checkbox/Checkbox.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Checkbox.css';
+
+export default function Checkbox() {
+  return (
+    <div
+      className='checkbox'
+      onClick={(e) => {
+        const checkbox = e.currentTarget;
+        checkbox.classList.contains('active') ? checkbox.classList.remove('active') : checkbox.classList.add('active');
+      }}
+    >
+      <svg width='12px' height='9px' viewBox='0 0 12 9'>
+        <polyline points='1 5 4 8 11 1'></polyline>
+      </svg>
+    </div>
+  );
+}
---
+++ src/component/toggle/Toggle.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Toggle.css';
+
+export default function Toggle() {
+  return (
+    <div
+      className='toggle'
+      onClick={(e) => {
+        const toggle = e.currentTarget;
+        toggle.classList.contains('active') ? toggle.classList.remove('active') : toggle.classList.add('active');
+      }}
+    >
+      <span></span>
+    </div>
+  );
+}
