4f631ddfa9c136d8ae05c4b45583e74aab8a520e
ByungHakNoh <nbh6579@naver.com>
2022-02-14 21:14:05 +0900

    [DEL] header : delete test code

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import './header.css';
 
-function clickTest(e: React.MouseEventHandler<HTMLElement>) {
-  console.log(e);
-}
-
 export default function Header() {
-  return <header onClick={clickTest}>header</header>;
+  return <header>header</header>;
 }
