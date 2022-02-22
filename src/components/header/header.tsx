f0f6db0e3bd8bd594acce9332f6b2e0a99777f35
weed082 <nbh6579@naver.com>
2022-02-23 00:03:55 +0900

    [FIX] websocket

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,14 +1,6 @@
 import React from 'react';
 import './header.css';
 
-const clickTest = (e: React.MouseEvent<HTMLElement>) => {
-  console.log(e);
-  const test = 'yes';
-  if (test === 'yes') {
-    console.log('yes yes');
-  }
-};
-
 export default function Header() {
-  return <header onClick={clickTest}>header</header>;
+  return <header>header</header>;
 }
--- src/model/chat.ts
+++ src/model/chat.ts
@@ -1,18 +1,33 @@
-type msg = { id: Number; name: String };
-export default class ChatModel {
-  private ws?: WebSocket = new WebSocket('http://localhost:5000/chat');
+type Req = { type: Number; body?: Msg };
+type Msg = {
+  type?: Number;
+  userIdx?: Number;
+  name?: String;
+  body?: String;
+};
 
+export default class ChatModel {
+  private ws = new WebSocket('ws://localhost:5000/chat');
   constructor() {
-    this.onConnect(); 
+    this.ws.onopen = this.onConnect.bind(this);
+    this.ws.onmessage = this.onReceiveMsg.bind(this);
   }
 
+  //* on connect to ws socket
   private onConnect() {
-    this.ws!.onopen = () => {
-      console.log('ws connected');
-      const msg: msg = { id: 2, name: 'tom' };
-      const jsonMsg = JSON.stringify(msg);
-      this.ws!.send(jsonMsg);
-    };
+    const msg: Msg = { name: 'tom' };
+    const req: Req = { type: 1, body: msg };
+    this.sendMsg(req);
+  }
+
+  //* on receive msg
+  private onReceiveMsg(e: MessageEvent) {
+    const msg = JSON.parse(e.data);
+    console.log(msg);
+  }
+
+  private sendMsg(msg: Req) {
+    const jsonMsg = JSON.stringify(msg);
+    this.ws.send(jsonMsg);
   }
 }
-
